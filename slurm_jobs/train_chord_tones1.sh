a6746bd72693ac3a031fbeb35c3bfc5c4624046e
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-09-14 13:42:54 -0400

    wip

--- slurm_jobs/train_chord_tones1.sh
+++ slurm_jobs/train_chord_tones1.sh
@@ -19,7 +19,8 @@ python /home/ms3682/code/musicbert_fork/training_scripts/train_chord_tones.py \
     -W chord_tones_musicbert \
     -c "${SAVED_CHECKPOINTS_DIR}"/musicbert_provided_checkpoints/checkpoint_last_musicbert_base.pt \
     -u 50000 \
-    -w 5000
+    -w 5000 \
+    --batch-size 32
 
 set +x
 
