27a52206f09e6c6665586e6d99011b9b11ef9487
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-09-18 16:12:24 -0400

    wip

---
+++ slurm_jobs/chord_tones_and_onset.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --job-name=train_cts
+#SBATCH --time=012:00:00
+#SBATCH --mail-type=ALL
+#SBATCH -o /home/ms3682/slurm_output/%j.out # Note that directory will not be created if it does not exist; also, ~ expansion doesn't seem to work
+
+module load miniconda
+conda activate newbert
+
+set -x
+
+python /home/ms3682/code/musicbert_fork/training_scripts/train_chord_tones.py \
+    -d /home/ms3682/project/datasets/chord_tones/fairseq/with_onsets_bin \
+    -a base \
+    -W chord_tones_and_onsets_musicbert \
+    "${@}"
+
+set +x
+
+# Equivalent command for local development:
+# bash train_chord_tones.sh \
+#     -d /Users/malcolm/output/fairseq/chord_tones_seqs_bin \
+#     -a base \
+#     -W scratch \
+#     -c /Volumes/Reicha/large_checkpoints/musicbert/checkpoint_last_musicbert_base.pt \
+#     -u 50000 \
+#     -w 10000
+
