fe2b00f1ba5cd276c4a712976101b750fcee2d5e
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-10-13 10:51:34 -0400

    update script

--- misc_scripts/local_upload.sh
+++ misc_scripts/local_upload.sh
@@ -7,6 +7,12 @@ if [[ -z $data_folder ]]; then
     exit 1
 fi
 
+if [[ -e "${data_folder}".zip ]]; then
+    set -x
+    trash "${data_folder}".zip
+    set +x
+fi
+
 set -x
 zip -r "${data_folder}".zip "${data_folder}"
 scp -r "${data_folder}".zip ms3682@transfer-grace.ycrc.yale.edu:project/raw_data/
