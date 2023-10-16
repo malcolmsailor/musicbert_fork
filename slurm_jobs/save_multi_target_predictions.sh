db8a6aa8b8b289434eda37fe65072ad18fda1375
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-10-16 10:30:19 -0400

    bugfix

--- slurm_jobs/save_multi_target_predictions.sh
+++ slurm_jobs/save_multi_target_predictions.sh
@@ -14,9 +14,9 @@ CHECKPOINT_BASE="/home/ms3682/project/new_checkpoints/musicbert_fork"
 if [[ -z "$1" ]]; then
     echo Usage: save_multi_target_predictions.sh [run_name]
     echo "   We look for a checkpoint in"
-    echo "       "$CHECKPOINT_BASE"/[run_name]/checkpoint_best.pt"
+    echo "       $CHECKPOINT_BASE/[run_name]/checkpoint_best.pt"
     echo "   We save predictions to "
-    echo "       "$PREDICTIONS_BASE"/[run_name]/"
+    echo "       $PREDICTIONS_BASE/[run_name]/"
     exit 1
 fi
 
@@ -31,7 +31,7 @@ set -x
 python /home/ms3682/code/musicbert_fork/eval_scripts/save_multi_target_predictions.py \
     --data-dir /home/ms3682/project/datasets/unlabeled/ycac_bin/ \
     --checkpoint "${CHECKPOINT_BASE}"/"${run_name}"/checkpoint_best.pt \
-     --output-folder "${PREDICTIONS_BASE}"/${run_name}/"
+     --output-folder "${PREDICTIONS_BASE}/${run_name}/"
     "${@}"
 
 set +x
