fe34083df72efb93cd41c79156905f22c6260bac
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-10-19 17:16:39 -0400

    update write_seqs

--- misc_scripts/write_seqs.sh
+++ misc_scripts/write_seqs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=write_seqs
-#SBATCH --time=2:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH -o /home/ms3682/slurm_output/%j.out # Note that directory will not be created if it does not exist; also, ~ expansion doesn't seem to work
 
 if [[ -z "$4" ]]; then
