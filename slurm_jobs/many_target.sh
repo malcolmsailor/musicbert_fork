592938d2d9683b7abdbb550a1778358c5ce0c371
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-11-29 15:10:44 -0500

    update many_target.sh

--- slurm_jobs/many_target.sh
+++ slurm_jobs/many_target.sh
@@ -8,13 +8,20 @@
 #SBATCH --mail-type=ALL
 #SBATCH -o /home/ms3682/slurm_output/%j.out # Note that directory will not be created if it does not exist; also, ~ expansion doesn't seem to work
 
+TASK=$1
+
+if [[ -z $TASK ]]; then
+    echo 'Usage: bash many_target.sh [TASK] [other args...]'
+    exit 1
+fi
+
 module load miniconda
 conda activate newbert
 
 set -x
 
 python /home/ms3682/code/musicbert_fork/training_scripts/train_chord_tones.py \
-    -d /home/ms3682/project/datasets/chord_tones/fairseq/many_target_bin \
+    -d /home/ms3682/project/datasets/chord_tones/fairseq/"$TASK"_bin \
     -a base \
     -W chord_tones_many_target \
     --multitarget \
