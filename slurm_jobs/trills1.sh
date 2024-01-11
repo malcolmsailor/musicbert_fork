0b081c1f6223adf8da6ab8dcef78342227281c4a
Malcolm Sailor <malcolm.sailor@gmail.com>
2024-01-11 14:20:27 -0500

    wip

--- slurm_jobs/trills1.sh
+++ slurm_jobs/trills1.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --job-name=train_cts
+#SBATCH --job-name=train_quantize
 #SBATCH --time=012:00:00
 #SBATCH --mail-type=ALL
 #SBATCH -o /home/ms3682/slurm_output/%j.out # Note that directory will not be created if it does not exist; also, ~ expansion doesn't seem to work
@@ -14,9 +14,9 @@ conda activate newbert
 set -x
 
 python /home/ms3682/code/musicbert_fork/training_scripts/train_chord_tones.py \
-    -d /home/ms3682/project/datasets/trill_bin \
+    -d /home/ms3682/project/datasets/quantize_data_bin \
     -a base \
-    -W trills \
+    -W quantize_classification \
     "${@}"
 
 set +x
