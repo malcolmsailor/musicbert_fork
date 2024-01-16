611af3aa78bc16e7a12614fcab4c8be4dc567ae6
Malcolm Sailor <malcolm.sailor@gmail.com>
2024-01-16 11:40:25 -0500

    wip

--- slurm_jobs/save_quantize_predictions.sh
+++ slurm_jobs/save_quantize_predictions.sh
@@ -31,7 +31,7 @@ conda activate newbert
 set -x
 
 python /home/ms3682/code/musicbert_fork/eval_scripts/save_multi_target_predictions.py \
-    --data-dir /home/ms3682/project/datasets/ycac_no_salami_slice_bin/ \
+    --data-dir /home/ms3682/project/datasets/ycac_unquantized_bin/ \
     --checkpoint "${CHECKPOINT_BASE}"/"${run_name}"/checkpoint_best.pt \
     --output-folder "${PREDICTIONS_BASE}/${run_name}/" \
     --ref-dir ~/project/datasets/"$data_base"_bin \
