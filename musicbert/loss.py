980b544af7c730b36fca17fc2b243aea24cbd272
Malcolm Sailor <malcolm.sailor@gmail.com>
2024-03-08 07:43:06 -0500

    wip

--- musicbert/loss.py
+++ musicbert/loss.py
@@ -2,8 +2,28 @@ import torch
 import torch.nn.functional as F
 
 
-def p_norm(x1: torch.Tensor, x2: torch.Tensor, p: float = 2.0):
-    return (x1 - x2).abs().pow(p).sum(axis=-1).pow(1 / p)  # type:ignore
+def p_norm(
+    x1: torch.Tensor,
+    x2: torch.Tensor,
+    p: float = 2.0,
+    min_eps: float = 1e-7,
+    max_eps: float = 1e2,
+):
+    # this calculation is highly likely to overflow with fp16
+    dtype = x1.dtype
+    x1 = x1.float()
+    x2 = x2.float()
+
+    diffs = (x1 - x2).abs()
+    # For numerical stability, we need to avoid infinities, which can happen
+    #   if these differences are somewhat large (because they are then raised to *p*
+    #   and then summed)
+    diffs = torch.where(diffs > max_eps, max_eps, diffs)
+    sum = diffs.pow(p).sum(axis=-1)  # type:ignore
+    out = (sum + min_eps).pow(1 / p)
+    return out.type(dtype)
+
+    # return (x1 - x2).abs().pow(p).sum(axis=-1).pow(1 / p)  # type:ignore
 
 
 def p_norm_loss(
@@ -16,7 +36,7 @@ def p_norm_loss(
     #   necessarily want. It seems that the appropriate way of doing this
     #   is just to take a softmax first.
     # logits have shape [..., seq, logits]
-    probs = F.softmax(logits, dim=-1)
+    probs = F.log_softmax(logits, dim=-1)
     norm = p_norm(probs[..., :-1, :], probs[..., 1:, :], p=p)
     # last dimension is gone; 2nd-last dimension reduced by 1
     assert norm.shape == (*shape[:-2], shape[-2] - 1)
