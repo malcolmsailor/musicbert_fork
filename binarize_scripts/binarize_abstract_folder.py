453b4aa9907a64a35e371fbd0e74b226d67b3982
Malcolm Sailor <malcolm.sailor@gmail.com>
2024-03-20 07:52:54 -0400

    wip

---
+++ binarize_scripts/binarize_abstract_folder.py
@@ -0,0 +1,88 @@
+from collections import defaultdict
+from itertools import chain
+import os
+import re
+import subprocess
+import glob
+
+import sys
+from dataclasses import dataclass
+from omegaconf import OmegaConf
+
+
+@dataclass
+class Config:
+    input_folder: str
+    workers: int = 24
+    inputs_name: str = "midi"
+
+
+conf = OmegaConf.from_cli(sys.argv[1:])
+config = Config(**conf)  # type:ignore
+
+assert re.search(r"_raw/?$", config.input_folder)
+
+output_folder = config.input_folder.rstrip(os.path.sep)[:-4] + "_bin"
+
+assert not os.path.exists(output_folder)
+
+assert config.workers > 0
+
+files = [
+    os.path.basename(f)
+    for f in chain(
+        glob.glob(os.path.join(config.input_folder, "*_train.txt")),
+        glob.glob(os.path.join(config.input_folder, "*_valid.txt")),
+        glob.glob(os.path.join(config.input_folder, "*_test.txt")),
+    )
+]
+
+srcdict_path = os.path.join(config.input_folder, "dict.input.txt")
+assert os.path.exists(srcdict_path)
+
+inputs = set()
+keyed_features = defaultdict(set)
+for f in files:
+    feature = f.rsplit("_", maxsplit=1)[0]
+    if feature == config.inputs_name:
+        inputs.add(f)
+    else:
+        keyed_features[feature].add(f)
+
+assert inputs
+
+
+def do_feature(feature, files, output_subfolder, src_dict=None):
+    command = ["fairseq-preprocess", "--only-source"]
+    if f"{feature}_train.txt" in files:
+        command.extend(
+            ["--trainpref", os.path.join(config.input_folder, f"{feature}_train.txt")]
+        )
+    if f"{feature}_valid.txt" in files:
+        command.extend(
+            ["--validpref", os.path.join(config.input_folder, f"{feature}_valid.txt")]
+        )
+    if f"{feature}_test.txt" in files:
+        command.extend(
+            ["--testpref", os.path.join(config.input_folder, f"{feature}_test.txt")]
+        )
+    command.extend(
+        [
+            "--destdir",
+            os.path.join(output_folder, output_subfolder),
+            "--workers",
+            str(config.workers),
+        ]
+    )
+
+    if src_dict:
+        command.extend(["--srcdict", src_dict])
+
+    print("+ " + " ".join(command))
+    subprocess.run(command, check=True)
+
+
+do_feature("midi", inputs, "input0", srcdict_path)
+
+for key, files in keyed_features.items():
+    do_feature(key, files, key)
---
+++ misc_scripts/instantiate_abstract_dataset.py
@@ -0,0 +1,49 @@
+import json
+import os
+import sys
+from dataclasses import dataclass
+from typing import List, Optional
+from omegaconf import OmegaConf
+
+
+@dataclass
+class Config:
+    input_folder: str
+    feature_names: List[str]
+    output_folder: str
+    conditioning: Optional[str] = None
+    inputs_name: str = "input0"
+
+
+def make_links(feature, output_subfolder, config):
+    src_dir = os.path.join(config.input_folder, feature)
+    assert os.path.isdir(src_dir)
+    dst_dir = os.path.join(config.output_folder, output_subfolder)
+
+    print(f"Linking {dst_dir} -> {src_dir}")
+    os.symlink(src_dir, dst_dir)
+
+
+def main():
+    conf = OmegaConf.from_cli(sys.argv[1:])
+    config = Config(**conf)  # type:ignore
+
+    assert config.output_folder.endswith("_bin")
+    assert not os.path.exists(config.output_folder)
+    os.makedirs(config.output_folder)
+
+    with open(os.path.join(config.output_folder, "target_names.json"), "w") as outf:
+        json.dump(list(config.feature_names), outf)
+        print(f"Wrote {os.path.join(config.output_folder, 'target_names.json')}")
+
+    make_links(config.inputs_name, "input0", config)
+
+    for i, feature in enumerate(config.feature_names):
+        make_links(feature, f"label{i}", config)
+
+    if config.conditioning:
+        make_links(config.conditioning, "conditioning", config)
+
+
+if __name__ == "__main__":
+    main()
