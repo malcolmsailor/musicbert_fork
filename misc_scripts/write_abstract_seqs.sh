8cc619c5b5b351bcf326d04ff0075155a32b52ec
Malcolm Sailor <malcolm.sailor@gmail.com>
2024-03-20 13:45:24 -0400

    wip

--- misc_scripts/write_abstract_seqs.sh
+++ misc_scripts/write_abstract_seqs.sh
@@ -7,10 +7,12 @@
 #SBATCH -o /home/ms3682/slurm_output/%j.out # Note that directory will not be created if it does not exist; also, ~ expansion doesn't seem to work
 
 INPUT_DIR=$1
-INPUTS_PATHS=$2
-if [[ -z "$2" ]]; then
+INTERMEDIATE_DIR=$2 # E.g., ~/project/raw_data/abstract_seqs
+RAW_OUTPUT_DIR=$3   # E.g., ~/project/datasets/chord_tones/fairseq/abstract_raw
+INPUTS_PATHS=$4
+if [[ -z "$4" ]]; then
     echo Error: 2 positional arguments required.
-    echo Usage: bash write_abstract_seqs.sh [input_dir] [input_splits_paths]
+    echo Usage: bash write_abstract_seqs.sh [input_dir] [intermediate_dir] [output_dir] [input_splits_paths]
     exit 1
 fi
 
@@ -22,18 +24,18 @@ conda activate write_chord_tones_seqs
 set -x
 SRC_DATA_DIR=${INPUT_DIR} python -m write_seqs \
     --data-settings ~/code/write_seqs/configs/oct_data_abstract.yaml \
-    --output-dir ~/project/raw_data/abstract_seqs \
+    --output-dir ${INTERMEDIATE_DIR} \
     --input-paths-dir ${INPUTS_PATHS}
 
 python ~/code/write_seqs/scripts/to_fair_seq_abstract.py \
-    --input-dir ~/project/raw_data/abstract_seqs \
-    --output-dir ~/project/datasets/chord_tones/fairseq/abstract_raw
+    --input-dir ${INTERMEDIATE_DIR} \
+    --output-dir ${RAW_OUTPUT_DIR}
 set +x
 
 conda activate newbert
 
 set -x
 python ~/code/musicbert_fork/binarize_scripts/binarize_abstract_folder.py \
-    input_folder=~/project/datasets/chord_tones/fairseq/abstract_raw workers=16
+    input_folder=${RAW_OUTPUT_DIR} workers=16
 
 set +x
